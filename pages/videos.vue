d602afc9dad55a45092d1b35a3f5d855cde5ed0f
colekins <colekins@gmail.com>
2020-11-18 16:10:07 -0500

    Blank videos route for now

---
+++ pages/videos.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-container class="container content-container tour">
+    <div class="page-title">Videos</div>
+    <br />
+    <br />
+    <b-row>
+      <b-col> videos will be here </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  components: {
+    // AlbumCover,
+  },
+
+  data() {},
+}
+</script>
+
+<style></style>
