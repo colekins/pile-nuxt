96965a80d46035be5695187f84107ef996fdd232
colekins <colekins@gmail.com>
2020-11-18 17:20:42 -0500

    Delete buggy page

--- pages/contact.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <b-container class="container content-container contact">
-    <div class="page-title">Contact</div>
-    <br />
-    <br />
-    <b-row> </b-row>
-  </b-container>
-</template>
